d62858f6dadfdb858451a16aa313b5ca6d6e9bda
mara1esh <valerasheligan@gmail.com>
2020-02-27 19:19:42 +0200

    added pagging to table's grid

--- src/containers/Winners.tsx
+++ src/containers/Winners.tsx
@@ -2,13 +2,26 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import WinnersTable from "components/WinnersTable";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
-const Winners: React.FC<{}> = () => (
-  <Grid container justify="center">
-    <Grid item>
-      <Typography variant="h6">Winners</Typography>
-    </Grid>
-    <WinnersTable />
-  </Grid>
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {
+      paddingRight: 20
+    }
+  })
 );
+
+const Winners: React.FC<{}> = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container justify="center" className={classes.container}>
+      <Grid item>
+        <Typography variant="h6">Winners</Typography>
+      </Grid>
+      <WinnersTable />
+    </Grid>
+  );
+};
 export default Winners;
